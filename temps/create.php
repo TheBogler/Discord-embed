1f9e83bf3eb154e879c72b82424f21ab70a0edb2
TheBogler <101922574+TheBogler@users.noreply.github.com>
2023-04-21 18:34:09 +0300

    Add files via upload

---
+++ temps/create.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+<html>
+  <title>TBSELF</title>
+  <h1>Создать <b>embedded</b></h1>
+  <link rel="stylesheet" type="text/css" href="style.css">
+<form action="/" type="GET">
+  <input name="title" placeholder="Имя"><br>
+  <input name="desc" placeholder="Описание"><br>
+  <input name="color" placeholder="Цвет(HTML)"><br>
+  <button type="submit">Создать</button>
+</form>
+</html>
